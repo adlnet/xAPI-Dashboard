3abe3e580e2ebc61010248d1820a7057e7371394
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-04-10 15:48:24 -0400

    Added groupBy, sum, count, average, and getValue

--- set.js
+++ set.js
@@ -5,14 +5,19 @@
 
 function Set(arr)
 {
-	this._contents = arr ? arr.slice() : [];
+	this.contents = [];
+	for(var i in arr){
+		if( this.contents.indexOf(arr[i]) === -1 ){
+			this.contents.push(arr[i]);
+		}
+	}
 }
 
 // simple filter
 Set.prototype.select = function(filter)
 {
 	return new Set(
-		this._contents.reduce(
+		this.contents.reduce(
 			function(sum,val){
 				if( filter(val) )
 					sum.push(val);
@@ -26,10 +31,10 @@ Set.prototype.select = function(filter)
 Set.prototype.intersect = function(set)
 {
 	var ret = new Set();
-	for(var i in this._contents){
-		var obj = this._contents[i];
+	for(var i in this.contents){
+		var obj = this.contents[i];
 		if( set.contains(obj) )
-			ret._contents.push(obj);
+			ret.contents.push(obj);
 	}
 
 	return ret;
@@ -38,8 +43,8 @@ Set.prototype.intersect = function(set)
 // returns union of this and argument
 Set.prototype.union = function(set)
 {
-	var ret = new Set(this._contents);
-	ret._contents.push.apply(ret._contents, set._contents);
+	var ret = new Set(this.contents);
+	ret.contents.push.apply(ret.contents, set.contents);
 	return ret.selectDistinct();
 };
 
@@ -47,20 +52,37 @@ Set.prototype.union = function(set)
 Set.prototype.without = function(set)
 {
 	var ret = new Set();
-	for(var i in this._contents){
-		var obj = this._contents[i];
+	for(var i in this.contents){
+		var obj = this.contents[i];
 		if( !set.contains(obj) )
-			ret._contents.push(obj);
+			ret.contents.push(obj);
 	}
 
 	return ret;
 }
 
+// aggregate and run callback
+Set.prototype.groupBy = function(xpath, cb)
+{
+	var pathVals = this.selectDistinct(xpath).transform(Set.getValue(xpath)).contents;
+	var ret = new Set();
+	for( var i in pathVals )
+	{
+		var val = pathVals[i];
+		ret.contents.push({
+			'groupValue': val,
+			'result': cb(this.selectEqual(xpath, val))
+		});
+	}
+
+	return ret;
+};
+
 // order the contents of the set by path
 Set.prototype.orderBy = function(xpath, comparator)
 {
 	var parts = xpath ? xpath.split('.') : [];
-	var sortFn = comparator;
+	var sortFn;
 	if( /^(?:ascending|descending)$/i.test(comparator) ){
 		sortFn = function(a,b){
 			if(comparator === 'descending'){
@@ -75,8 +97,11 @@ Set.prototype.orderBy = function(xpath, comparator)
 			}
 		};
 	}
+	else {
+		sortFn = comparator;
+	}
 
-	this._contents.sort(function(a,b)
+	this.contents.sort(function(a,b)
 	{
 		var curA=a, curB=b;
 		for(var i in parts){
@@ -94,29 +119,60 @@ Set.prototype.orderBy = function(xpath, comparator)
 				return 0;
 			}
 		}
-		return comparator(curA,curB);
+		return sortFn(curA,curB);
 	});
 	return this;
 };
 
-// the number of elements in the set
-Set.prototype.count = function(){
-	return this._contents.length;
+// the number of elements in the set (or subset)
+Set.prototype.count = function(xpath)
+{
+	if(xpath){
+		return this.selectDistinct(xpath).count();
+	}
+	else {
+		return this.contents.length;
+	}
 }
+
+// the total of a particular field
+Set.prototype.sum = function(xpath)
+{
+	var parts = xpath ? xpath.split('.') : [];
+	var sum = 0;
+	for( var i in this.contents )
+	{
+		var elem = this.contents[i];
+		for( var j=0; j<parts.length; j++)
+		{
+			if( elem[parts[j]] )
+				elem = elem[parts[j]];
+			else
+				break;
+		}
+		if( j === parts.length ){
+			sum += elem;
+		}
+	}
+
+	return sum;
+}
+
+// the average of a particular field
+Set.prototype.average = function(xpath)
+{
+	return this.sum(xpath)/this.count();
+};
 
 // whether or not a particular item is in the set
 Set.prototype.contains = function(item){
-	return this._contents.indexOf(item) !== -1;
+	return this.contents.indexOf(item) !== -1;
 }
 
 // returns a new set with xfrm applied to each element
 // xfrm takes (element, index, origArray)
 Set.prototype.transform = function(xfrm){
-	return new Set( this._contents.map(xfrm) );
-};
-
-Set.prototype.distinctCount = function(xpath){
-	return this.select(Set.distinct(xpath)).count();
+	return new Set( this.contents.map(xfrm) );
 };
 
 Set.prototype.selectDistinct = function(xpath){
@@ -173,4 +229,27 @@ Set.equals = function(xpath, value)
 		}
 		return curElem === value;
 	}
+};
+
+// return elem s.t. startVal <= elem < endVal
+Set.between = function(xpath, startVal, endVal)
+{
+	
+};
+
+
+// transform to get xpath values out of a set
+Set.getValue = function(xpath)
+{
+	var parts = xpath ? xpath.split('.') : [];
+	return function(elem){
+		var curElem = elem;
+		for(var i=0; i<parts.length; i++){
+			if(curElem[parts[i]])
+				curElem = curElem[parts[i]];
+			else
+				return null;
+		}
+		return curElem;
+	};
 };
