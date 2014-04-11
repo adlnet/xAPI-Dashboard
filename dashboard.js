319f93a01238df42588959d6b198a9ee5dedce04
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-04-11 11:11:23 -0400

    Restructured dashboard object

--- dashboard.js
+++ dashboard.js
@@ -1,34 +1,13 @@
 "use strict";
 
-var ADL = ADL || {};
-ADL.XAPIDashboard = new (function(){
-	
-	var self = this;
-	
-	self.chartType;
-	self.raphael;
-	self.set;
-	
-	self.x;
-	self.y;	
-	self.width;
-	self.height;
+(function(ADL){
 
-	self.init = function(type, x, y, width, height){		
-		
-		self.x = x ? x : 0;
-		self.y = y ? y : 0;
-		self.height = height ? height : 0;
-		self.width = width ? width : 0;
-		
-		// Creates canvas 640 × 480 at 10, 50
-		//self.raphael = Raphael(self.x, self.y, self.width, self.height);
-		//self.chartType = /^(piechart|dotchart|linechart|barchart)$/.test(type) ? type : 'barchart';
-		self.set = new Set();
-	};
-	
-	self.getAllStatements = function(query, cb){
-	
+	var XAPIGraph = function(type, container){
+		this.set = new Set();
+	}
+
+	XAPIGraph.prototype.fetchAllStatements = function(query, cb){
+		var self = this;
 		ADL.XAPIWrapper.getStatements(query, null, function getMore(r){
 			var response = JSON.parse(r.response);
 			self.addStatements(response.statements);
@@ -44,9 +23,9 @@ ADL.XAPIDashboard = new (function(){
 	};
 	
 	//To-do
-	self.clearSavedStatements = function(){};
+	XAPIGraph.prototype.clearSavedStatements = function(){};
 	
-	self.addStatements = function(statementsArr){
+	XAPIGraph.prototype.addStatements = function(statementsArr){
 		if(statementsArr.response){
 			try{
 				statementsArr = JSON.parse(statementsArr.response).statements;
@@ -57,39 +36,30 @@ ADL.XAPIDashboard = new (function(){
 			}
 		}
 
-		self.set = self.set.union(new Set(statementsArr));
+		this.set = this.set.union(new Set(statementsArr));
 	};
 	
-	self.sum = function(xAPIFieldA, xAPIFieldB){
+	XAPIGraph.prototype.sum = function(xAPIFieldA, xAPIFieldB){
 	
 		return [];
 	};
 	
-	self.count = function(xAPIField, pre, post){
-		var b = self.set;
+	XAPIGraph.prototype.count = function(groupField, labelField, pre, post){
+		var b = this.set;
 		if(pre)
 			b = pre(b);
 
-		b = b.groupBy(xAPIField, function(groupSet){ return {
-			'token': groupSet.contents[0],
+		b = b.groupBy(groupField, function(groupSet){ return {
+			'sample': groupSet.contents[0],
 			'count': groupSet.count()
 		}});
 
 		if(post)
 			b = post(b);
 
-		/*
-		var data = [b.contents.map(function(element){
-			return element.result;
-		})];
-
-		self.raphael[self.chartType](self.x, self.y, self.width, self.height, data);
-		var axis = Raphael.g.axis(85,230,310,null,null,4,2,['today','yesterday','tomorrow','future'], '|',0);
-		*/
-
 		nv.addGraph(function(){
 			var chart = nv.models.discreteBarChart()
-				.x(function(d){ return d.result.token.object.definition.name['en-US']; })
+				.x(function(d){ return new Set([d.result.sample]).transform(Set.getValue(labelField)).contents[0]; })
 				.y(function(d){ return d.result.count; })
 				.showXAxis(false)
 				.staggerLabels(true)
@@ -105,5 +75,8 @@ ADL.XAPIDashboard = new (function(){
 			nv.utils.windowResize(chart.update);
 			return chart;
 		});
-	}	
-})();
+	}
+
+	ADL.XAPIGraph = XAPIGraph;
+
+})(window.ADL = window.ADL || {});
