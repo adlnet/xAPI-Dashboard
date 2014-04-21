ca79b469eac29f0b80ee60a5bc09d8009b24b0af
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-04-21 10:26:38 -0400

    Finished test data generator

--- generateData/generateData.py
+++ generateData/generateData.py
@@ -1,11 +1,17 @@
 #!/bin/env python
-import random, json
+import random, json, datetime as dt
+from uuid import uuid4
 
 random.seed()
 nameData = []
 with open('names.json','r') as names:
 	nameData = json.load(names)
 
+class EST(dt.tzinfo):
+	def utcoffset(self, d):
+		return dt.timedelta(hours=-5)
+	def dst(self, d):
+		return dt.timedelta(hours=1)
 
 
 class Student(object):
@@ -18,12 +24,6 @@ class Student(object):
 
 		self.average = average
 		self.deviation = deviation
-
-	def __hash__(self):
-		return self.email
-
-	def __eq__(self, other):
-		return hash(self) == hash(other)
 
 	def answerQuestion(self, difficulty):
 		return random.normalvariate(self.average,self.deviation) > difficulty
@@ -38,33 +38,132 @@ class Class(object):
 			studentDeviation = 15
 			self.students.append( Student( studentAverage, studentDeviation ) )
 
-	def takeTest(self, testId, questionDifficulties):
-		results = TestResults(testId, len(questionDifficulties))
-		for q,d in enumerate(questionDifficulties):
-			for s in self.students:
-				results.logAnswer( s, q, s.answerQuestion(d) )
+	def takeTest(self, test):
+		results = TestResults(len(test))
+		for questionNum,difficulty in enumerate(test):
+			for student in self.students:
+				results.logAnswer( student, questionNum, student.answerQuestion(difficulty) )
 
 		return results
 
 
-class TestResults(object):
-	def __init__(self, testId, numQuestions):
-		self.testId = testId
-		self.questionResults = [{} for i in range(numQuestions)]
+class Test(list):
+	def __init__(self, name, questions):
+		self.name = name
+		for q in questions:
+			self.append(q)
+
+
+class Battery(object):
+	def __init__(self):
+		self.tests = []
+
+	def run(self, group):
+		result = {}
+		for test in self.tests:
+			result[test.name] = group.takeTest(test)
+
+		return result
+
+
+class TestResults(list):
+	def __init__(self, numQuestions):
+		for i in range(numQuestions):
+			self.append({})
 
 	def logAnswer(self, student, questionNumber, success):
-		self.questionResults[questionNumber][student] = success
+		self[questionNumber][student] = success
+
+
+def genStatements(results):
+
+	xapiStatements = []
+
+	for testid,questions in results.items():
+
+		times = {}
+		averages = {}
+		try:
+			startTime += dt.timedelta(hours=3)
+		except UnboundLocalError:
+			startTime = dt.datetime.now(EST())
+
+		for qNum,qResults in enumerate(questions):
+
+			for student,result in qResults.items():
+
+				if qNum == 0:
+					xapiStatements.append( genStatement(student,'attempted',testid,startTime) )
+
+				activity = '{}/q{}'.format(testid,qNum)
+				times[student] = times.get(student, startTime) + dt.timedelta(seconds=random.randint(30,90))
+
+				xapiStatements.append( genStatement(student,'answered',activity,times[student],result) )
+
+				averages[student] = (averages.get(student, 0) * qNum + (100 if result else 0))/(qNum+1)
+
+		for student,time in times.items():
+			xapiStatements.append( genStatement(student,'completed',testid,time) )
+			passed = 'passed' if averages[student] > 60 else 'failed'
+			xapiStatements.append( genStatement(student,passed,testid,time,averages[student]) )
+
+	return xapiStatements
+
+			
+def genStatement(student, verb, activity, time, score=None):
+	stmt = {
+		'actor': {
+			'name': student.name,
+			'mbox': 'mailto:'+student.email
+		},
+		'verb': {
+			'id': 'http://adlnet.gov/expapi/verbs/'+verb,
+			'display': {'en-US': verb}
+		},
+		'object': {
+			'id': 'http://myschool.edu/xapi_vocab/'+activity,
+			'definition': {
+				'name': activity
+			}
+		},
+
+		'authority': {
+			'mbox': 'mailto:admin@myschool.edu',
+			'objectType': 'Agent'
+		},
+		'timestamp': time.isoformat(),
+		'stored': time.isoformat(),
+		'version': '1.0.1',
+		'id': str(uuid4())
+	}
+
+	if isinstance(score, bool):
+		stmt['result'] = {
+			'success': score
+		}
+	elif isinstance(score, int):
+		stmt['result'] = {
+			'score': {
+				'raw': score,
+				'min': 0,
+				'max': 100
+			}
+		}
+	
+	return stmt
 
 
 
 def main():
-	#student = Student(75,20)
-	#print student.name, student.email
-	#for i in range(10):
-	#	print student.answerQuestion(50)
-	myclass = Class(10, 85, 20)
-	results = myclass.takeTest('test1', [25,25,25,50,50,50])
-	print results.questionResults
+
+	battery = Battery()
+	battery.tests.append( Test('test1', [25,25,25,50,50,50]) )
+
+	myclass = Class(10, 80, 20)
+
+	results = battery.run(myclass)
+	statements = genStatements(results)
+	print json.dumps(statements, indent=4)
 
 if __name__ == '__main__':
 	main()
