864e2d01dbdf48173a2c63d781288fccde4e7a2a
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2014-05-15 09:29:41 -0400

    Updated build script to minify collection

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,33 +1,38 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-	concat: {
-		options: {
-			separator: ';'
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['lib/d3.v3.js', 'lib/nv.d3.js', 'lib/xapiwrapper.min.js','src/dashboard.js', 'src/chart.js'],
+				dest: 'dist/xapidashboard.js'
+			}
 		},
-		dist: {
-			src: ['lib/d3.v3.js', 'lib/nv.d3.js', 'lib/xapiwrapper.min.js','src/dashboard.js', 'src/chart.js'],
-			dest: 'dist/xapidashboard.js'
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> v<%= pkg.version %> | Built on <%= grunt.template.today("mm-dd-yyyy") %> */\n'
+			},
+			build: {
+				files: [{
+					src: 'dist/xapidashboard.js',
+					dest: 'dist/xapidashboard.min.js'
+				},{
+					src: 'src/xapiCollection.js',
+					dest: 'dist/xapiCollection.min.js'
+				}]
+			}
 		}
-	},
-    uglify: {
-      options: {
-        banner: '/*! <%= pkg.name %> v<%= pkg.version %> | Built on <%= grunt.template.today("mm-dd-yyyy") %> */\n'
-      },
-      build: {
-        src: 'dist/xapidashboard.js',
-        dest: 'dist/xapidashboard.min.js'
-      }
-    }
-  });
+	});
 
-  // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-concat');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	// Load the plugin that provides the "uglify" task.
+	grunt.loadNpmTasks('grunt-contrib-concat');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
 
-  // Default task(s).
-  grunt.registerTask('default', ['concat', 'uglify']);
+	// Default task(s).
+	grunt.registerTask('default', ['concat', 'uglify']);
 
 };
