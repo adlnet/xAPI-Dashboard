ea85eba7dfcd88bcf2d3a4197c694d28b115b18b
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2015-04-02 17:22:13 -0400

    Added xpath resolution, couple processors

--- src/xapistream.js
+++ src/xapistream.js
@@ -65,8 +65,118 @@
 	Processor._procmap = {};
 
 
+	/********************************************
+	 * XPath evaluation functions
+	 ********************************************/
 
 
+	/*
+	 * Retrieves some deep value at path from an object
+	 */
+	function getVal(path,obj)
+	{
+		// if nothing to search, return null
+		if(obj === undefined){
+			return null;
+		}
+
+		// if no descent, just return object
+		else if(path.length === 0){
+			return obj;
+		}
+
+		else {
+
+			// break xpath into individual keys
+			var parts;
+			if(Array.isArray(path)){
+				parts = path;
+			}
+			else {
+				parts = path.split('.');
+				for(var i=0; i<parts.length;){
+					if(parts[i].slice(-1) === '\\')
+						parts.splice(i, 2, parts[i].slice(0,-1)+'.'+parts[i+1]);
+					else
+						i++;
+				}
+			}
+
+			// fetch deep path
+			var scoped = parts[0], rest = parts.slice(1);
+			if( scoped === '*' )
+			{
+				var ret = [], keys = [];
+
+				if(Array.isArray(obj)){
+					for(var i=0; i<obj.length; i++) keys.push(i);
+				}
+				else {
+					keys = Object.keys(obj);
+				}
+
+				for(var i=0; i<keys.length; i++){
+					var keyout = getVal(rest, obj[keys[i]]);
+					if(Array.isArray(keyout))
+						ret.push.apply(ret,keyout);
+					else
+						ret.push(keyout);
+				}
+				
+				return ret;
+			}
+			else {
+				return getVal(rest, obj[scoped]);
+			}
+		}
+	}
+
+	/*
+	 * Set some deep value in an object
+	 */
+	function setVal(obj,path,value)
+	{
+		// break xpath into individual keys
+		var parts;
+		if(Array.isArray(path)){
+			parts = path;
+		}
+		else {
+			parts = path.split('.');
+			for(var i=0; i<parts.length;){
+				if(parts[i].slice(-1) === '\\')
+					parts.splice(i, 2, parts[i].slice(0,-1)+'.'+parts[i+1]);
+				else
+					i++;
+			}
+		}
+
+		if(!obj){
+			obj = {};
+		}
+
+		if(parts.length === 1){
+			obj[parts[0]] = value;
+		}
+		else {
+			if(obj[parts[0]] !== undefined)
+				obj[parts[0]] = setVal(obj[parts[0]], parts.slice(1), value);
+			else
+				obj[parts[0]] = setVal({}, parts.slice(1), value);
+		}
+
+		return obj;
+	}
+
+
+	/****************************************************************
+	 * Standard processor factories
+	 ****************************************************************/
+
+	// simply print out the input
+	// @args
+	// 	prefix (str): a string to be prefixed to the console output
+	//
 	function ConsoleLogger(prefix)
 	{
 		return new Processor(function(data)
@@ -83,10 +193,64 @@
 		});
 	}
 
+	// remove unwanted items from input stream
+	// @args
+	// 	selector (function(data,[index],[array])):
+	// 		if selector returns true for item, will be in output
+	//
+	function Filter(selector)
+	{
+		return new Processor(function(data)
+		{
+			if( Array.isArray(data) )
+			{
+				var ret = [];
+				for( var i=0; i<data.length; i++ ){
+					if( selector(data[i], i, data) ){
+						ret.push( data[i] );
+					}
+				}
+				return ret;
+			}
+			else
+			{
+				if( selector(data, null, null) ){
+					return data;
+				}
+			}
+		});
+	}
+
+
+	function FilterNotNull(xpath){
+		return Filter(function(item){
+			return getVal(xpath, item) !== null;
+		}
+	}
+
+	function FilterEQ(xpath, value){
+		return Filter(function(item){
+			return getVal(xpath, item) === item;
+		}
+	}
+
+	function FilterDistinct(xpath){
+		
+	}
+	
+	function GroupByDistinct(xpath)
+	{
+		
+	}
+
+
 
 	ADL.XAPIStream = {
 		'Processor': Processor,
-		'ConsoleLogger': ConsoleLogger
+		'ConsoleLogger': ConsoleLogger,
+		'Filter': Filter,
+		'FilterNotNull': FilterNotNull,
+		'FilterEQ': FilterEQ
 	};
 
 })(window.ADL = window.ADL || {});
