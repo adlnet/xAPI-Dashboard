21fc12df5160d4d59362c91b3c4fc1e43fa80463
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2015-07-07 09:27:46 -0400

    Removed a couple console.log statements in Collection

--- src/xapicollection.js
+++ src/xapicollection.js
@@ -78,7 +78,7 @@ if(!String.prototype.reverse){
 				// pull the next part from the front of the path
 				// if bracket, read to close bracket. if no bracket, read to dot or open bracket
 				var match = /^(?:([^\[\.]+)|\[([^\]]+)\])\.?/.exec(subpath);
-				console.log(match);
+				//console.log(match);
 				if( !match ){
 					console.error('Invalid xpath:', path);
 					return null;
@@ -89,7 +89,7 @@ if(!String.prototype.reverse){
 					subpath = subpath.slice(match[0].length);
 				}
 			}
-			console.log(parts);
+			//console.log(parts);
 		}
 
 		return parts;
